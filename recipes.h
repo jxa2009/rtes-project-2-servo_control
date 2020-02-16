fbbc90e31e08e1e9ae25a4b732ecbe952764302d
Jason Au <jxa2009@g.rit.edu>
2020-02-15 19:31:46 -0500

    Added project documentation and created recipes files.

---
+++ recipes.h
@@ -0,0 +1,13 @@
+#ifndef RECIPES_H
+#define RECIPES_H
+
+#define MOV         (0x20)
+#define WAIT        (0x40)
+#define LOOP        (0x80)
+#define END_LOOP    (0xA0)
+#define END_RECIPE   (0x00)
+
+
+
+
+#endif
