2a0002c051f92c1e74a077937cf29ebef897dbc2
Jason Au <jxa2009@g.rit.edu>
2020-02-16 18:12:42 -0500

    Added more to recipes

--- recipes.c
+++ recipes.c
@@ -1,3 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "recipes.h""
+#include <stdint.h>
+#include "recipes.h"
+
+
+
+/**
+ * Function that moves a given servo to positions 0 to 5. Any others will fail.
+ * Inputs:
+ *          [uint32_t] *servo - pointer to a given servo (1 or 2)
+ *          [int] positions - Number 0 to 5 to move the servo
+ * Outputs:
+ *          Returns 1 if successfully moved, 0 if failed
+ * */
+int move_servo_to_position( uint32_t *servo, int position)
+{
+    // Might have to change positions ? servo is pointer to uin32
+    if (position < 0 || position > 5)
+    {
+        return 0;
+    }
+    switch(position)
+    {
+        case 0:
+            servo = POSITION_0;
+            break;
+        case 1:
+            servo = POSITION_1;
+            break;
+        case 2:
+            servo = POSITION_2;
+            break;
+        case 3:
+            servo = POSITION_3;
+            break;
+        case 4:
+            servo = POSITION_4;
+            break;
+        case 5:
+            servo = POSITION_5;
+            break;
+        default:
+            return 0;
+    }
+    return 1;
+}
+
+
+
--- recipes.h
+++ recipes.h
@@ -1,13 +1,60 @@
 #ifndef RECIPES_H
 #define RECIPES_H
 
+// Different recipe commands
 #define MOV         (0x20)
 #define WAIT        (0x40)
 #define LOOP        (0x80)
 #define END_LOOP    (0xA0)
 #define END_RECIPE   (0x00)
 
+// Pulse width time for positions (in ms) (steps of .32ms)
+// Rounded for now
+#define POSITION_0 (20)
+#define POSITION_1 (17)//(16.8)
+#define POSITION_2 (14)// (13.6)
+#define POSITION_3 (10)
+#define POSITION_4 (7)
+#define POSITION_5 (4)
+// Recipes are statically compiled into the program
+unsigned char recipe1[] = {};
+unsigned char recipe2[] = {};
 
 
+// Pointer to the recipes for ease of access
+unsigned char *recipes[] = {recipe1, recipe2, NULL};
+
+// Global State value
+// Initial State as paused
+static Servo_StateE current_servo_state = Status_Paused;
+// Enum for statuses, that will be used to glow up the LEDs
+typedef enum Status_E
+{
+    Status_Running,
+    Status_Paused,
+    Status_Command_Error,
+    Status_Nested_Error
+} StatusE;
+
+typedef enum Servo_State_E
+{
+    State_At_Position,
+    State_Unknown,
+    State_Moving,
+    State_Recipe_Ended
+} Servo_StateE;
+
+//Event Transistions between states
+typedef enum Events_E
+{
+    User_Paused_Recipe,
+    User_Continued_Recipe,
+    User_Entered_Left,
+    User_Entered_Right,
+    User_Begin_Restart_Recipe,
+    Recipe_Ended
+} EventsE;
+
+int move_servo_to_position( uint32_t *servo,int position);
 
 #endif
